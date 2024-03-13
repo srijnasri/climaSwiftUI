866c06af2f59d3bb311d27a15d90aefb1ab58904
srijnasri <srijnasri1999@gmail.com>
2024-03-13 07:23:06 +0530

    Initial Commit

---
+++ climaSwiftUI/climaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  climaSwiftUIApp.swift
+//  climaSwiftUI
+//
+//  Created by Srijnasri Negi on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct climaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
